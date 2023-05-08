0b0a9da89471c061c3f2dfab50b255f58c13e4ae
Alex <ph0bo@protonmail.com>
2023-05-08 12:51:10 +0300

    middleware comment removal

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,3 @@
-// This middleware is to protect the proxied api to be called only by the same host
-// In the future it could get more logic, for example JWT tokens, etc.
-
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getIronSession } from "iron-session/edge";
@@ -10,18 +7,8 @@ export const middleware = async (req: NextRequest) => {
   const res = NextResponse.next();
   const session = await getIronSession(req, res, sessionOptions);
 
-  // do anything with session here:
+
   const { user } = session;
-
-  // like mutate user:
-  // user.something = someOtherThing;
-  // or:
-  // session.user = someoneElse;
-
-  // uncomment next line to commit changes:
-  // await session.save();
-  // or maybe you want to destroy session:
-  // await session.destroy();
   
   if (req.nextUrl.pathname !== '/') {
     if (user?.isLoggedIn) {
@@ -31,25 +18,5 @@ export const middleware = async (req: NextRequest) => {
     }
   }
 
-  // if (req.nextUrl.pathname === '/tasks') {
-  //   if (user?.isLoggedIn) {
-  //     return res;
-  //   } else {
-  //     return NextResponse.redirect(new URL('/', req.url))  
-  //   }
-  // }
-
-  // if (req.nextUrl.pathname === '/task') {
-  //   if (user?.isLoggedIn) {
-  //     return res;
-  //   } else {
-  //     return NextResponse.redirect(new URL('/', req.url))  
-  //   }
-  // }
-
 };
 
-
-export const config = {
-  matcher: ['/tasks', '/task'],
-};
