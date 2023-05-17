917ebb5a56b1842fb9bef004b3594c9f85af0d0b
Alex <ph0bo@protonmail.com>
2023-05-17 09:45:50 +0300

    middleware iron-session fix

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getIronSession } from "iron-session/edge";
-import { sessionOptions } from "./lib/session";
+import type { IronSessionOptions } from "iron-session";
 
 export const middleware = async (req: NextRequest) => {
   const res = NextResponse.next();
+
+  const sessionOptions: IronSessionOptions = {
+    password: process.env.SECRET_COOKIE_PASSWORD as string,
+    cookieName: "iron-session/s4f",
+    cookieOptions: {
+       secure: false //process.env.NODE_ENV === "production", TODO: set to true when SSL
+    },
+  };
   const session = await getIronSession(req, res, sessionOptions);
 
-
   const { user } = session;
-  
+
   if (req.nextUrl.pathname !== '/') {
     if (user?.isLoggedIn) {
       return res;
@@ -17,10 +24,4 @@ export const middleware = async (req: NextRequest) => {
       return NextResponse.redirect(new URL('/', req.url))  
     }
   }
-
 };
-
-export const config = {
-  matcher: "/",
-};
-
