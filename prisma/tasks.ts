958694dc894db4a76e833b132d3090012c26fddd
Alex <ph0bo@protonmail.com>
2023-12-14 13:27:33 +0200

    new default migration tasks

--- prisma/tasks.ts
+++ prisma/tasks.ts
@@ -1,14 +1,22 @@
 export const tasks = [
     {
-        name: "Task 1",
-        description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+        name: "Clean up the place! ",
+        description: "Managing a gym means you're the jack of all traders. Before anything else, you need to make sure that the gym floor, equipment and lockers are spotless!"
     },
     {
-        name: "Task 2",
-        description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+        name: "Check the Equipment!",
+        description: "Once you've finished cleaning, it's time to check the equipment and restock the shop. Visually and functionally inspect each piece of equipment, make sure all of the weights are on the racks and if you find any problems, call maintenance."
     },
     {
-        name: "Task 3",
-        description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+        name: "Brief your staff!",
+        description: "Every daily meeting with all of the staff is a great way to make sure your team is up to date on everything that's working right and everything that isn't great. It's a great way to sync, gather and impart feedback."
+    },
+    {
+        name: "Make room for Marketing!",
+        description: "It's time you have a look at your marketing initiatives and how well they're working. This is a time to let your imagination run wild and figure out new  ways to grow the gym."
+    },
+    {
+        name: "Admin Time!",
+        description: "At the end of the day, it's time to see how well the gym is doing. Check your client and employee feedback, make your payroll, pay your suppliers and check your accounts!"
     }
 ]
